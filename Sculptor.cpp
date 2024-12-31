de32837e4a7ec55df487d825ce49099f0f396c82
Alvaro <alvinhofd@gmail.com>
2024-12-31 15:13:57 -0300

    Update Sculptor.cpp
    concluido.

--- Sculptor.cpp
+++ Sculptor.cpp
@@ -25,7 +25,7 @@ r = g = b = a = 0;
   for(int i = 0; i < ny; i++){
     for(int j = 0; j < nx; j++){ 
       for(int k = 0; k < nz; k++){
-        v[i][j][k].isOn = false; // inicializar cada elemento como vazio, ou falso
+        v[i][j][k].show = false; // inicializar cada elemento como vazio, ou falso
 
          v[i][j][k].r = r;
          v[i][j][k].g = g;
@@ -59,7 +59,7 @@ a = sa;
 } 
 // ativa o voxel
 void Sculptor :: putVoxel(int px, int py, int pz){ // p = put
-v[px][py][pz].isOn = true;
+v[px][py][pz].show = true;
 v[px][py][pz].r = r;
 v[px][py][pz].g = g;
 v[px][py][pz].b = b;
@@ -67,14 +67,14 @@ v[px][py][pz].a = a;
 }
 //desativa o voxel
 void Sculptor :: cutVoxel(int cx, int cy, int cz){ // c = cut
-v[cx][cy][cz].isOn = false;
+v[cx][cy][cz].show = false;
 }
 // faz uma caixa, ou seja, põe voxels em um determinado intervalo 
 void Sculptor :: putBox(int x0, int x1, int y0, int y1, int z0, int z1){
   for(int i = x0; i < x1; i++){
     for(int j = y0; j < y1; j++){ 
       for(int k = z0; k < z1; k++){
-        v[i][j][k].isOn = true; //ativa
+        v[i][j][k].show = true; //ativa
          v[i][j][k].r = r;
          v[i][j][k].g = g;
          v[i][j][k].b = b;
@@ -83,15 +83,19 @@ void Sculptor :: putBox(int x0, int x1, int y0, int y1, int z0, int z1){
     }
   }
 }  
+
 // tira essa caixa
 void Sculptor :: cutBox(int x0_, int x1_, int y0_, int y1_, int z0_, int z1_){
   for(int i = x0_; i < x1_; i++){
     for(int j = y0_; j < y1_; j++){ 
       for(int k = z0_; k < z1_; k++){
-        v[i][j][k].isOn = false; //desativa
+        v[i][j][k].show = false; //desativa
       }
     }
   }
+}
+
+
   // faz uma bola, ou como o nome diz, uma esfera em um determinado intervalo
   void Sculptor :: putSphere(int xc, int yc, int zc, int r){ // c = center
   double x2, y2, z2; // uso o tipo double para aumentarmos a precisão do intervalo
@@ -102,7 +106,7 @@ void Sculptor :: cutBox(int x0_, int x1_, int y0_, int y1_, int z0_, int z1_){
           y2 = (double)(j - yc)*(double)(j - yc);
           z2 = (double)(k - zc)*(double)(k - zc);
           if((x2 + y2 + z2) < (r*r)){
-            v[i][j][k].isOn = true;
+            v[i][j][k].show = true;
              v[i][j][k].r = r;
              v[i][j][k].g = g;
              v[i][j][k].b = b;
@@ -122,7 +126,7 @@ void Sculptor :: cutBox(int x0_, int x1_, int y0_, int y1_, int z0_, int z1_){
             y2 = (double)(j - yc_)*(double)(j - yc_);
             z2 = (double)(k - zc_)*(double)(k - zc_);
             if((x2_ + y2_ + z2_) < (r_*r_)){
-              v[i][j][k].isOn = false;
+              v[i][j][k].show = false;
             }
           }
         }
@@ -138,7 +142,7 @@ void Sculptor :: cutBox(int x0_, int x1_, int y0_, int y1_, int z0_, int z1_){
             ye = (double)(i - yc)*(double)(i - yc)/(ry*ry);
             ze = (double)(i - zc)*(double)(i - zc)/(rz*rz);
             if((xe + ye + ze) < 1){
-              v[i][j][k].isOn = true;
+              v[i][j][k].show = true;
                v[i][j][k].r = r;
                v[i][j][k].g = g;
                v[i][j][k].b = b;
@@ -158,7 +162,7 @@ void Sculptor :: cutBox(int x0_, int x1_, int y0_, int y1_, int z0_, int z1_){
             ye = (double)(i - yc_)*(double)(i - yc_)/(ry_*ry_);
             ze = (double)(i - zc_)*(double)(i - zc_)/(rz_*rz_);
             if((xe + ye + ze) < 1){
-              v[i][j][k].isOn = false;
+              v[i][j][k].show = false;
             }
           }
         }
@@ -166,10 +170,72 @@ void Sculptor :: cutBox(int x0_, int x1_, int y0_, int y1_, int z0_, int z1_){
   }
   // metodo para salvar arquivos ;) 
   void Sculptor :: writeOFF(const char* filename){
-    ofstream fout;
-    fixed(fout); // correção da precisão do .off
-    int nver, nface, aux;
-    nver = nface = aux = 0;
-    fout.open(filename);
-    fout<<"OFF"<<endl;
-  }
+    std::ofstream ArqOFF;
+    fixed(ArqOFF); // correção da precisão do .off nas casas decimais
+    int Nvox = 0, aux;
+    ArqOFF.open(filename);
+    
+    
+    //criação do nosso arquivo:
+    
+      ArqOFF<<"OFF\n";
+    
+    for(int i = 0; i < ny; i++){
+      for(int j = 0; j < nx; j++){ 
+        for(int k = 0; k < nz; k++){
+          if(v[i][j][k].show == true){
+            Nvox++;
+          }
+        }
+      }
+    }
+
+    ArqOFF<<Nvox*8<<" "<<Nvox*6<<" "<<0<<"\n"; // quantidade de vertices e de faces, 0 arestas pois não precisa indicar.
+
+    // hora de colocar os valores de acordo com as orientações para o voxel ser um cubo
+    for(int a = 0; i < ny; a++){
+      for(int b = 0; j < nx; b++){ 
+        for(int c = 0; k < nz; c++){
+          if(v[a][b][c].show == true){
+            ArqOFF << x - 0.5 << " " << y - 0.5 << " " << z - 0.5 << std::endl<< std::flush;
+            ArqOFF << x - 0.5 << " " << y + 0.5 << " " << z - 0.5 << std::endl<< std::flush;
+            ArqOFF << x + 0.5 << " " << y + 0.5 << " " << z - 0.5 << std::endl<< std::flush;
+            ArqOFF << x + 0.5 << " " << y - 0.5 << " " << z - 0.5 << std::endl<< std::flush;
+            ArqOFF << x - 0.5 << " " << y - 0.5 << " " << z + 0.5 << std::endl<< std::flush;
+            ArqOFF << x - 0.5 << " " << y + 0.5 << " " << z + 0.5 << std::endl<< std::flush;
+            ArqOFF << x + 0.5 << " " << y + 0.5 << " " << z + 0.5 << std::endl<< std::flush;
+            ArqOFF << x + 0.5 << " " << y - 0.5 << " " << z + 0.5 << std::endl<< std::flush;
+      }
+     }
+   }
+ }
+    
+Nvox = 0;
+    for(int i = 0; i < ny; a++){
+      for(int j = 0; j < nx; b++){ 
+        for(int k = 0; k < nz; c++){
+          if(v[i][j][k].show == true){
+            aux = Nvox*8;
+              ArqOFF<<4<<" "<<aux+0<<" "<<aux+3<<" "<<aux+2<<" "<<aux+1<<" "<<v[i][j][k].r<<" "
+              <<v[i][j][k].g<<" "<<v[i][j][k].b<<" "<<v[i][j][k].a<<std::endl;
+              ArqOFF<<4<<" "<<aux+4<<" "<<aux+5<<" "<<aux+6<<" "<<aux+7<<" "<<v[i][j][k].r<<" "
+              <<v[i][j][k].g<<" "<<v[i][j][k].b<<" "<<v[i][j][k].a<<std::endl;
+              ArqOFF<<4<<" "<<aux+0<<" "<<aux+1<<" "<<aux+5<<" "<<std::aux+4<<" "<<v[i][j][k].r<<" "
+              <<v[i][j][k].g<<" "<<v[i][j][k].b<<" "<<v[i][j][k].a<<std::endl;
+              ArqOFF<<4<<" "<<aux+0<<" "<<aux+4<<" "<<aux+7<<" "<<aux+3<<" "<<v[i][j][k].r<<" "
+              <<v[i][j][k].g<<" "<<v[i][j][k].b<<" "<<v[i][j][k].a<<std::endl;
+              ArqOFF<<4<<" "<<aux+3<<" "<<aux+7<<" "<<aux+6<<" "<<aux+2<<" "<<v[i][j][k].r<<" "
+              <<v[i][j][k].g<<" "<<v[i][j][k].b<<" "<<v[i][j][k].a<<std::endl;
+              ArqOFF<<4<<" "<<aux+1<<" "<<aux+2<<" "<<aux+6<<" "<<aux+5<<" "<<v[i][j][k].r<<" "
+              <<v[i][j][k].g<<" "<<v[i][j][k].b<<" "<<v[i][j][k].a<<std::endl;
+    
+                          Nvox = Nvox+8;
+                        }
+                    }
+                }
+            }
+    if(ArqOFF.is_open()){
+            std::cout << "Arquivo.OFF salvo!"<<std::endl;
+        }
+  ArqOFF.close();
+}
