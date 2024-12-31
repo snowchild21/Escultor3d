c0deee9caf9cb875f8131942824b5e7fe661f72f
Alvaro <alvinhofd@gmail.com>
2024-12-31 15:16:52 -0300

    Create teste.cpp

---
+++ teste.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include "voxel.h"
+#include "sculptor.h"
+#include "Sculptor.cpp"
+
+int main(){
+
+Sculptor caixa(100, 100, 100);
+caixa.setColor(0.28, 0.81, 0.79, 0.7);
+caixa.putBox(0, 10, 0, 10, 0, 10);
+caixa.WriteOFF("caixa ciano.off");
+return 0;  
+}
