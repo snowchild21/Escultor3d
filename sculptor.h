d4ce7f486364d6c0d4ec1f13328af6b55f5fc25a
Alvaro <alvinhofd@gmail.com>
2025-01-02 17:53:15 -0300

    Update sculptor.h

--- sculptor.h
+++ sculptor.h
@@ -1,15 +1,15 @@
 #ifndef SCULPTOR_H
 #define SCULPTOR_H
+
 #include "voxel.h"
 class Sculptor {
 private:
-  Voxel ***v; // Vetor da matriz 3D
-  int nx,ny,nz; // Dimensões
-  float r,g,b,a; // Propriedades do voxel
-float fix;
+  Voxel ***v; // 3D matrix
+  int nx,ny,nz; // Dimensions
+  float r,g,b,a; // Current drawing color
 public:
-  Sculptor(int _nx, int _ny, int _nz); // construtor
-  ~Sculptor(); // destrutor
+  Sculptor(int _nx, int _ny, int _nz);
+  ~Sculptor();
   void setColor(float r, float g, float b, float alpha);
   void putVoxel(int x, int y, int z);
   void cutVoxel(int x, int y, int z);
